8ef5666b67dae9412dc5765189cd459d091b9610
breadsroofses <yaroslavhorovyi@gmail.com>
2024-07-22 16:31:40 +0300

    be1

---
+++ main.py
@@ -0,0 +1,76 @@
+import random
+
+from PyQt6.QtWidgets import *
+
+import database
+
+app = QApplication([])
+window = QWidget()
+menu_btn = QPushButton("Меню")
+relax_btn = QPushButton("Відпочити")
+timer_sbx = QSpinBox()
+min_lbl = QLabel("хвилин")
+quest_lbl = QLabel("2+2?")
+res_lbl = QLabel("")
+var1_btn = QRadioButton("1")
+var2_btn = QRadioButton("2")
+var3_btn = QRadioButton("3")
+var4_btn = QRadioButton("4")
+answer_btn = QPushButton("Відповісти")
+next_quest_btn = QPushButton("Наступне запитання")
+group = QGroupBox("Варіанти відповідей")
+
+
+main_line = QVBoxLayout()
+
+horizontal_line1 = QHBoxLayout()
+horizontal_line1.addWidget(menu_btn)
+horizontal_line1.addStretch(1)
+horizontal_line1.addWidget(relax_btn)
+horizontal_line1.addWidget(timer_sbx)
+horizontal_line1.addWidget(min_lbl)
+horizontal_line1.addWidget(answer_btn)
+main_line.addLayout(horizontal_line1)
+main_line.addWidget(quest_lbl)
+
+group_main_line = QVBoxLayout()
+group_main_line.addWidget(var1_btn)
+group_main_line.addWidget(var2_btn)
+group_main_line.addWidget(var3_btn)
+group_main_line.addWidget(var4_btn)
+group.setLayout(group_main_line)
+main_line.addWidget(group)
+main_line.addWidget(next_quest_btn)
+
+answers = [var1_btn, var2_btn, var3_btn, var4_btn]
+
+def set_quest():
+    random.shuffle(answers)
+    random.shuffle(answers)
+    current_question = database.questions[database.nomer]
+    quest_lbl.setText(current_question["запитання"])
+    answers[0].setText(current_question["Правильна відповідь"])
+    answers[1].setText(current_question["Не правильна відповідь1"])
+    answers[2].setText(current_question["Не правильна відповідь2"])
+    answers[3].setText(current_question["Не правильна відповідь3"])
+set_quest()
+
+def ans_func():
+    if answers[0].isChecked():
+        res_lbl.setText("Правильно")
+    else:
+        res_lbl.setText("не правильно")
+    answers[0].hide()
+    answers[1].hide()
+    answers[2].hide()
+    answers[3].hide()
+    res_lbl.show()
+    next_quest_btn.show()
+    answer_btn.hide()
+
+answer_btn.clicked.connect(ans_func)
+
+
+window.setLayout(main_line)
+window.show()
+app.exec()
